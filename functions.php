83f9350e9f2817f66da7ee61537c1e487acceb0a
itoyasutoshi <gan.sun1100@gmail.com>
2017-05-16 10:30:42 +0900

    バリデーションとセキュリティ

--- functions.php
+++ functions.php
@@ -1,5 +1,28 @@
 <?php
   require_once('connection.php');
+  session_start();
+
+  function h($s) {
+    return htmlspecialchars($s, ENT_QUOTES, "UTF-8");
+  }
+
+  function setToken() {
+    $token = sha1(uniqid(mt_rand(), true));
+    $_SESSION['token'] = $token;
+  }
+
+  function checkToken($data) {
+    if (empty($_SESSION['token']) || ($_SESSION['token'] != $data)) {
+      $_SESSION['err'] = '不正な操作です';
+      header('location : '.$_SERVER['HTTP_REFERER'].'');
+      exit();
+    }
+    return true;
+  }
+
+  function unsetSession() {
+    if(!empty($_SESSION['err'])) $_SESSION['err'] = '';
+  }
 
   function checkReferer() {
     $httpArr = parse_url($_SERVER['HTTP_REFERER']);
@@ -7,10 +30,14 @@
   }
 
   function transition($path) {
+    unsetSession(); //追記
     $data = $_POST;
+    if(isset($data['todo'])) $res = validate($data['todo']); // 追記
     if($path === '/index.php' && $data['type'] === 'delete'){
       deleteData($data['id']);
       return 'index';
+    }elseif(!$res || !empty($_SESSION['err'])){ // 追記
+      return 'back';  // 追記
     }elseif($path === '/new.php'){
       create($data);
       return 'index';
@@ -20,8 +47,14 @@
     }
   }
 
+  function validate($data) {
+    return $res = $data != "" ? true : $_SESSION['err'] = '入力がありません';
+  }
+
   function create($data) {
-    insertDb($data['todo']);
+    if(checkToken($data['token'])) {
+      insertDb($data['todo']);
+    }
   }
 
   function index() {
@@ -29,7 +62,9 @@
   }
 
   function update($data) {
-    updateDb($data['id'], $data['todo']);
+    if (checkToken($data['token'])) {
+      updateDb($data['id'], $data['todo']);
+    }
   }
 
   function detail($id) {
--- store.php
+++ store.php
@@ -2,7 +2,10 @@
   require('functions.php');
 
   $res = checkReferer();
-  if($res == 'index'){
+  $res = checkReferer();
+  if($res != 'back'){
+    header('location: ./index.php');
+  }elseif($res == 'index'){
     header('location: ./index.php');
   }else{
     header('location: '.$_SERVER['HTTP_REFERER'].'');
